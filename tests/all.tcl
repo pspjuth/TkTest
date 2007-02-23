e94a2b9d7d28dacd206ba9ef26a794e445709f08
peter <peter>
2007-02-23 05:04:44 +0000

    Added files

---
+++ tests/all.tcl
@@ -0,0 +1,24 @@
+#!/bin/sh
+#----------------------------------------------------------------------
+# $Revision: 1.1 $
+#----------------------------------------------------------------------
+# the next line restarts using tclsh \
+exec tclsh "$0" "$@"
+
+set thisScript [file normalize [file join [pwd] [info script]]]
+set thisDir    [file dirname $thisScript]
+
+package require tcltest
+namespace import tcltest::*
+tcltest::configure -verbose "body error" -singleproc 1
+lappend ::auto_path [file join $thisDir]/../src
+package require TkTest
+
+if {$argc > 0} {
+    eval tcltest::configure $argv
+}
+
+tcltest::testsDirectory $thisDir
+tcltest::runAllTests
+
+cleanupTestFile
